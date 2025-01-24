38c5191bcc45e134fc92c44c8e243beb241b7804
Gabriel Turcin <gabriel10tm@gmail.com>
2025-01-24 22:32:13 +0200

    added stuf

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -20,10 +20,11 @@ public class PlayerController : MonoBehaviour
     Vector2 currentVelocity;
 
     float targetSpearGunRotation;
+    float targetRotation;
     void UpdateSpearGunRotation()
     {
-        var dif = (Input.mousePosition - spearGun.transform.position);
-        targetSpearGunRotation = Mathf.Atan2(dif.y,dif.x) * Mathf.Rad2Deg;
+        var dif = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - spearGun.transform.position);
+        targetSpearGunRotation = (flipped ? (Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg + 180) : (-Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg )); 
         var eu = spearGun.transform.localEulerAngles;
         eu.z = targetSpearGunRotation;
         spearGun.transform.localEulerAngles = eu;   
